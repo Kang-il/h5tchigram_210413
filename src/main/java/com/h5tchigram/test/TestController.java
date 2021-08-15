974ba16d0e685013bffa3222777cd3da130b2133
NOSICKK\nosickk <h5tchi@naver.com>
2021-08-15 21:32:40 +0900

    Kang-il/h5tchigram_210413#1 초기 세팅 및 DB연동 테스트

--- src/main/java/com/h5tchigram/test/TestController.java
+++ src/main/java/com/h5tchigram/test/TestController.java
@@ -1,15 +1,35 @@
 package com.h5tchigram.test;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.h5tchigram.test.bo.TestBO;
+
 @Controller
 public class TestController {
 
+	@Autowired
+	private TestBO testBO;
+	
 	@RequestMapping("/test")
 	@ResponseBody
 	public String helloWorld() {
 		return "Hello World!";
+	}
+	
+	@RequestMapping("/test_db")
+	@ResponseBody
+	public Map<String, Object> testDB(){
+		Map<String, Object> result=testBO.getTestDB();
+		return result;
+	}
+	
+	@RequestMapping("/test_jsp")
+	public String testJsp() {
+		return "test/test";
 	}
 }
